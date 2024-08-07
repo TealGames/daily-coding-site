f1089b845f0dc679bfe04b296b07ff73dd32f05c
Teal Games <128750307+TealGames@users.noreply.github.com>
2024-08-06 20:56:04 -0500

    Added test project files

---
+++ proj/js/AnimateHeader.js
@@ -0,0 +1,22 @@
+
+(function animateTitle() {
+    const $animatingIdentifier = $("#title-char-identifier");
+    const onTransTime = 0.5;
+    const offTransTime = 0.5;
+
+    $animatingIdentifier.css("opacity");
+    animationDecision();
+
+    function animationDecision() {
+        if ($animatingIdentifier.css("opacity") < 0.01) {
+            $animatingIdentifier.animate({
+                opacity: "1",
+            }, onTransTime * 1000, animationDecision);
+        }
+        else {
+            $animatingIdentifier.animate({
+                opacity: "0",
+            }, offTransTime * 1000, animationDecision);
+        }
+    }
+}());
