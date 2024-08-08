35ccdb4e8669eaca1580697617e1bc48adfa4ded
Teal Games <128750307+TealGames@users.noreply.github.com>
2024-08-07 19:03:48 -0500

    Improved layout, spacing

---
+++ proj/js/HelperFunctions.mjs
@@ -0,0 +1,23 @@
+export class HelperFunctions {
+    static clamp(value, min, max) {
+        return Math.max(Math.min(value, max), min);
+    }
+
+    static delay(seconds) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(`Delay of ${seconds} has successfully completed!`);
+            }, 1000 * seconds);
+        });
+    }
+
+    static addHtmlToStart(element, html) {
+        element.html(html + element.html());
+    }
+
+    static addHtmlToEnd(element, html) {
+        element.html(element.html() + html);
+    }
+}
+
+
---
+++ proj/js/TextDisplay.mjs
@@ -0,0 +1,28 @@
+import { HelperFunctions } from "./HelperFunctions.mjs";
+
+async function displayText(text, waitTime, displayCallback) {
+    const minTime = 0;
+    const maxTime = 1;
+
+    waitTime = HelperFunctions.clamp(waitTime, minTime);
+    let foundTag = false;
+    for (let i = 0; i < text.length; i++) {
+        const character = text.charAt(i);
+
+        //Using type coercion empty strings are false
+        if (!character || character === " ") continue;
+
+        if (character === "<") {
+            foundTag = true;
+            continue;
+        }
+        else if (character === ">" && foundTag) {
+            foundTag = false;
+            continue;
+        }
+        else if (foundTag) continue;
+
+        await HelperFunctions.delay(waitTime);
+        displayCallback(character);
+    }
+}
